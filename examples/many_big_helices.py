66921bec6c6d73facd673e34bbf4fecfe44da829
David Doty <pexatus@gmail.com>
2019-08-14 00:01:09 -0700

    changed examples

--- examples/many_big_helices.py
+++ examples/many_big_helices.py
@@ -10,7 +10,7 @@ def main():
     for i in range(num_helices):
         helices.append(sc.Helix(i, max_bases))
     design = sc.DNADesign(helices=helices, strands=[], grid=sc.square)
-    # design.write_to_file("output_designs/many-big-helices.dna")
+    design.write_to_file("output_designs/many-big-helices.dna")
     # design.write_to_file("output_designs/few-small-helices.dna")
     # design.write_to_file("output_designs/few-small-helices-11-bases.dna")
     # design.write_to_file("output_designs/1-helix-3-bases.dna")
--- examples/many_helices.py
+++ examples/many_helices.py
@@ -6,7 +6,7 @@ def main():
     for i in range(30):
         helices.append(sc.Helix(i, 5000))
     design = sc.DNADesign(helices=helices, strands=[], grid=sc.square)
-    design.write_to_file("output_designs/large-helices.dna")
+    design.write_to_file("output_designs/many-helices.dna")
 
 if __name__ == "__main__":
     main()
