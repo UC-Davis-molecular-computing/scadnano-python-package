d37e3af94e86e67056921fda6486a9ba5c3f6e31
tcosmo <tristan.sterin@ens-lyon.fr>
2019-11-22 10:27:36 +0000

    doc fixed

--- doc/conf.py
+++ doc/conf.py
@@ -14,7 +14,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../scadnano'))
+sys.path.insert(0, os.path.abspath('../'))
 # print(sys.path)
 
 # Type "make html" at the command line to generate the documentation.
