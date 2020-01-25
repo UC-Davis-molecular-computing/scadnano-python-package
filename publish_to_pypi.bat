b2a5e2201164b3ba7aeb9b3a1c7f5ba9f9e551ca
Dave Doty <doty@ucdavis.edu>
2020-01-24 17:52:25 -0800

    set up PyPi files

---
+++ publish_to_pypi.bat
@@ -0,0 +1,4 @@
+rem https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+
+python setup.py sdist
+twine upload dist/*
--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from distutils.core import setup
 
 setup(name='scadnano',
       packages=['scadnano'],
-      version='0.0.1',
+      version='0.1.0',
       license='MIT',
       description="Python scripting library for generating designs readable by scadnano.",
       author="David Doty",
       author_email="doty@ucdavis.edu",
       url="https://github.com/UC-Davis-molecular-computing/scadnano-python-package",
-      download_url = 'https://github.com/UC-Davis-molecular-computing/scadnano-python-package/???', 
+      download_url = 'https://github.com/UC-Davis-molecular-computing/scadnano-python-package/archive/v0.1.0.zip', 
     )
