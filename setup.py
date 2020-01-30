b6e0092b1e7c2ba5e6bd423913c3cf6623b2cbe7
Dave Doty <doty@ucdavis.edu>
2020-01-30 09:00:15 -0800

    bumped version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(name='scadnano',
       packages=['scadnano'],
-      version='0.1.1',
+      version='0.1.2',
       license='MIT',
       description="Python scripting library for generating designs readable by scadnano.",
       author="David Doty",
