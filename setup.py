3113cb919b0def5b5794dca5fa13873cd119cc71
David Doty <doty@ucdavis.edu>
2020-05-21 18:19:16 -0700

    bumped version number to 0.6.5, fixed #46, fixed #23

--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# from distutils.core import setup
+
+# got some ideas from here: https://medium.com/@joel.barmettler/how-to-upload-your-python-package-to-pypi-65edc5fe9c56
+#
+# But apparently setuptools is the replacement for distutils, and distutils was causing problems such as
+# not including the README.md file and not formatting it as Markdown on PyPI
+# https://setuptools.readthedocs.io/en/latest/setuptools.html
+
+from setuptools import setup, find_packages
 
 import scadnano
 
@@ -20,5 +28,6 @@ setup(name='scadnano',
       url="https://github.com/UC-Davis-molecular-computing/scadnano-python-package",
       download_url = 'https://github.com/UC-Davis-molecular-computing/scadnano-python-package/archive/v0.1.0.zip',
       long_description = long_description,
-      long_description_content_type = 'text/markdown', requires=['xlwt']
+      long_description_content_type = 'text/markdown; variant=GFM',
+      requires=['xlwt']
       )
