419caeb637de5587157915b2af73576b20ae504d
David Doty <doty@ucdavis.edu>
2020-07-25 10:47:55 -0700

    bumped version for breaking change (removed helix_template and num_helices from Design constructor parameters)

--- scadnano/scadnano.py
+++ scadnano/scadnano.py
@@ -44,7 +44,7 @@ so the user must take care not to set them.
 # commented out for now to support Python 3.6, which does not support this feature
 # from __future__ import annotations
 
-__version__ = "0.10.3"  # version line; WARNING: do not remove or change this line or comment
+__version__ = "0.11.0"  # version line; WARNING: do not remove or change this line or comment
 
 import dataclasses
 from abc import abstractmethod, ABC
