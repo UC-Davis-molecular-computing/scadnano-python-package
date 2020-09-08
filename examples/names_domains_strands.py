a2a6b072a21f83578c9aea5802eb25cc7e5eb829
David Doty <doty@ucdavis.edu>
2020-09-08 08:14:52 -0700

    updated names example to have more kinds of mismatches

--- examples/names_domains_strands.py
+++ examples/names_domains_strands.py
@@ -1,11 +1,14 @@
 import scadnano as sc
 
 
-def create_design():
-    num_domains = 5
-    helices = [sc.Helix(max_offset=num_domains * 8)]
+def create_design() -> sc.Design:
+    num_domains = 9
+    helices = [sc.Helix(max_offset=(num_domains) * 8)]
     design = sc.Design(helices=helices, strands=[], grid=sc.square)
-    design.strand(0, num_domains * 8) \
+    design.strand(0, (num_domains - 1) * 8) \
+        .move(-8).with_domain_name('domain 8*') \
+        .move(-8).with_domain_name('domain 7*') \
+        .move(-8).with_domain_name('domain 6*') \
         .move(-8).with_domain_name('domain 5*') \
         .move(-8).with_domain_name('domain 4*') \
         .move(-8).with_domain_name('domain 3*') \
@@ -14,19 +17,25 @@ def create_design():
         .with_name('bottom strand')
 
     # domain names match
-    design.strand(0, 0) .move(8).with_domain_name('domain 1') .with_name('top strand 1')
+    design.strand(0, 0).move(8).with_domain_name('domain 1').with_name('top strand 1')
 
     # domains are aligns but names mismatch
-    design.strand(0, 8) .move(8).with_domain_name('domain2') .with_name('top strand 2')
+    design.strand(0, 8).move(8).with_domain_name('domain 50').with_name('top strand 2')
 
     # domain names match
-    design.strand(0, 16) .move(8).with_domain_name('domain 3') .with_name('top strand 3')
+    design.strand(0, 16).move(8).with_domain_name('domain 3').with_name('top strand 3')
+
+    # domain names are the same, not complementary
+    design.strand(0, 24).move(8).with_domain_name('domain 4*').with_name('top strand 4')
 
     # domain names match but domains are mis-aligned
-    design.strand(0, 24) .move(9).with_domain_name('domain 4') .with_name('top strand 4')
+    design.strand(0, 32).move(9).with_domain_name('domain 5').with_name('top strand 5')
 
     # domain names match but domains are mis-aligned
-    design.strand(0, 33) .move(7).with_domain_name('domain 5') .with_name('top strand 5')
+    design.strand(0, 48).move(7).with_domain_name('domain 7').with_name('top strand 7')
+
+    # domain names match but domains are mis-aligned
+    design.strand(0, 64).move(8).with_domain_name('domain 9').with_name('top strand 9')
 
     return design
 
--- examples/output_designs/names_domains_strands.sc
+++ examples/output_designs/names_domains_strands.sc
@@ -9,6 +9,9 @@
       "name": "bottom strand",
       "color": "#f74308",
       "domains": [
+        {"name": "domain 8*", "helix": 0, "forward": false, "start": 56, "end": 64},
+        {"name": "domain 7*", "helix": 0, "forward": false, "start": 48, "end": 56},
+        {"name": "domain 6*", "helix": 0, "forward": false, "start": 40, "end": 48},
         {"name": "domain 5*", "helix": 0, "forward": false, "start": 32, "end": 40},
         {"name": "domain 4*", "helix": 0, "forward": false, "start": 24, "end": 32},
         {"name": "domain 3*", "helix": 0, "forward": false, "start": 16, "end": 24},
@@ -27,7 +30,7 @@
       "name": "top strand 2",
       "color": "#888888",
       "domains": [
-        {"name": "domain2", "helix": 0, "forward": true, "start": 8, "end": 16}
+        {"name": "domain 50", "helix": 0, "forward": true, "start": 8, "end": 16}
       ]
     },
     {
@@ -41,14 +44,28 @@
       "name": "top strand 4",
       "color": "#333333",
       "domains": [
-        {"name": "domain 4", "helix": 0, "forward": true, "start": 24, "end": 33}
+        {"name": "domain 4*", "helix": 0, "forward": true, "start": 24, "end": 32}
       ]
     },
     {
       "name": "top strand 5",
       "color": "#320096",
       "domains": [
-        {"name": "domain 5", "helix": 0, "forward": true, "start": 33, "end": 40}
+        {"name": "domain 5", "helix": 0, "forward": true, "start": 32, "end": 41}
+      ]
+    },
+    {
+      "name": "top strand 7",
+      "color": "#03b6a2",
+      "domains": [
+        {"name": "domain 7", "helix": 0, "forward": true, "start": 48, "end": 55}
+      ]
+    },
+    {
+      "name": "top strand 9",
+      "color": "#7300de",
+      "domains": [
+        {"name": "domain 9", "helix": 0, "forward": true, "start": 64, "end": 72}
       ]
     }
   ]
