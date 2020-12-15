5b1d08421fc6aa1c66f209dd393012e023dff9cc
David Doty <doty@ucdavis.edu>
2020-12-14 16:06:58 -0800

    bumped version to match web interface

--- scadnano/scadnano.py
+++ scadnano/scadnano.py
@@ -44,7 +44,7 @@ so the user must take care not to set them.
 # commented out for now to support Py3.6, which does not support this feature
 # from __future__ import annotations
 
-__version__ = "0.13.1"  # version line; WARNING: do not remove or change this line or comment
+__version__ = "0.13.4"  # version line; WARNING: do not remove or change this line or comment
 
 import dataclasses
 from abc import abstractmethod, ABC, ABCMeta
